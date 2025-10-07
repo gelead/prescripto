e152131cdb2640150b64bb6499e6daf5ca4aa866
Gelead <Geleadw@gmail.com>
2025-10-07 17:37:29 +0300

    login UI on Admin panel

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const AdminContext = createContext();
+
+const AdminContextProvider = (props) => {
+  const [token, setToken] = useState("");
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const value = {
+    token,
+    setToken,
+    backendUrl,
+  };
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+export default AdminContextProvider;
